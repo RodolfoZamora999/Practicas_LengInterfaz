ebba79451fd6d4da313268de1ccd7a7d59a8ee0b
Rodolfo Zamora <rodolfozamora999@gmail.com>
2020-10-16 20:52:01 -0700

    Segunda practica

--- Practica2.cpp
+++ Practica2.cpp
@@ -8,7 +8,8 @@ Repositorio: https://github.com/RodolfoZamora999/Practicas_LengInterfaz
 Fecha de revisión: 16/10/2020
 
 Objetivo del programa:
-Simular la caida de una moneda
+Es un hecho bien conocido que cada vez que se arroja una moneda al aire, hay una probabilidad de 50-50 que caiga "sol".
+Escribir un programa para imprimir una secuencia caracteristicas de 100 tiros (es decir, sol, aguila, sol, aguila, sol)
 */
 
 #include <iostream>
@@ -23,14 +24,17 @@ int main()
     srand((unsigned)time(0));
 
     //Generación de un número pseudoaleatorio
-    int value = rand() % 2;
+    for(int i = 0; i < 100; i++)
+    {
+        int value = rand() % 2;
 
-    if(value == 0)
-        resul = "Aguila";
-    else
-        resul = "Sello";
+        if(value == 0)
+            resul = "Aguila";
+        else
+            resul = "Sello";
 
-    cout << "Resultado: " << resul << endl;
+        cout <<"Tiro: " << (i + 1) << " Resultado: " << resul << endl;
+    }
 
     return 0;
 }
